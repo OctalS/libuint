73c0a47c0c8595d0238d1b4a729215607b0751e1
vlad_home <octal.s@gmail.com>
2013-07-20 23:19:14 +0300

    Corrected chunk sizes and incorrect values in libuint.h Testing a simple fnv hash

--- include/libuint.h
+++ include/libuint.h
@@ -13,12 +13,15 @@
 
 #define TOTAL_BITS	(CHUNK_BITS * N)
 
+#define	CPU_SIZE	CHUNK_SIZE == 4
 
-/*                                        n * 32bit entries | n * 64bit entries
-                                          -------------------------------------*/
-#define CHUNKS_128	(CHUNK_SIZE % 8 ?       4           :        2)
-#define CHUNKS_256	(CHUNK_SIZE % 8 ?       8           :        4)
-#define CHUNKS_1024	(CHUNK_SIZE % 8 ?       16          :        8)
+
+/*				  n * 32bit entries | n * 64bit entries
+				  -------------------------------------*/
+#define CHUNKS_128	(CPU_SIZE ?	4           :        2)
+#define CHUNKS_256	(CPU_SIZE ?	8           :        4)
+#define CHUNKS_512	(CPU_SIZE ?	16          :        8)
+#define CHUNKS_1024	(CPU_SIZE ?	32          :        16)
 
 typedef CHUNK_TYPE	uint128_t[CHUNKS_128];
 typedef CHUNK_TYPE	uint256_t[CHUNKS_256];
@@ -32,140 +35,170 @@ typedef CHUNK_TYPE	uint1024_t[CHUNKS_1024];
 
 #define	UINT128_MAX	"ffffffffffffffffffffffffffffffff"
 #define	UINT256_MAX	"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-#define	UINT1024_MAX	"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+#define	UINT512_MAX	"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+#define UINT1024_MAX	"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+
+
+
+#define	uint128_or(D, A, B)	__uintN_or(D, A, B, CHUNKS_128)
+#define	uint256_or(D, A, B)	__uintN_or(D, A, B, CHUNKS_256)
+#define	uint1024_or(D, A, B)	__uintN_or(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_or(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
+
+
+
+#define	uint128_or_s(D, A, B)	__uintN_or_s(D, A, B, CHUNKS_128)
+#define	uint256_or_s(D, A, B)	__uintN_or_s(D, A, B, CHUNKS_256)
+#define	uint1024_or_s(D, A, B)	__uintN_or_s(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_or_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
+
+
+
+#define	uint128_or_u(D, A, B)	__uintN_or_u(D, A, B, CHUNKS_128)
+#define	uint256_or_u(D, A, B)	__uintN_or_u(D, A, B, CHUNKS_256)
+#define	uint1024_or_u(D, A, B)	__uintN_or_u(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_or_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
 
 
 
 
-#define	uint128_or(D, A, B)		__uintN_or(D, A, B, CHUNKS_128)
-#define	uint256_or(D, A, B)		__uintN_or(D, A, B, CHUNKS_256)
-#define	uint1024_or(D, A, B)		__uintN_or(D, A, B, CHUNKS_1024)
+#define	uint128_xor(D, A, B)	__uintN_xor(D, A, B, CHUNKS_128)
+#define	uint256_xor(D, A, B)	__uintN_xor(D, A, B, CHUNKS_256)
+#define	uint1024_xor(D, A, B)	__uintN_xor(D, A, B, CHUNKS_1024)
 
-#define	uint128_or_s(D, A, B)		__uintN_or_s(D, A, B, CHUNKS_128)
-#define	uint256_or_s(D, A, B)		__uintN_or_s(D, A, B, CHUNKS_256)
-#define	uint1024_or_s(D, A, B)		__uintN_or_s(D, A, B, CHUNKS_1024)
-
-#define	uint128_or_u(D, A, B)		__uintN_or_u(D, A, B, CHUNKS_128)
-#define	uint256_or_u(D, A, B)		__uintN_or_u(D, A, B, CHUNKS_256)
-#define	uint1024_or_u(D, A, B)		__uintN_or_u(D, A, B, CHUNKS_1024)
-
-#define	uint128_xor(D, A, B)		__uintN_xor(D, A, B, CHUNKS_128)
-#define	uint256_xor(D, A, B)		__uintN_xor(D, A, B, CHUNKS_256)
-#define	uint1024_xor(D, A, B)		__uintN_xor(D, A, B, CHUNKS_1024)
-
-#define	uint128_xor_s(D, A, B)		__uintN_xor_s(D, A, B, CHUNKS_128)
-#define	uint256_xor_s(D, A, B)		__uintN_xor_s(D, A, B, CHUNKS_256)
-#define	uint1024_xor_s(D, A, B)		__uintN_xor_s(D, A, B, CHUNKS_1024)
-
-#define	uint128_xor_u(D, A, B)		__uintN_xor_u(D, A, B, CHUNKS_128)
-#define	uint256_xor_u(D, A, B)		__uintN_xor_u(D, A, B, CHUNKS_256)
-#define	uint1024_xor_u(D, A, B)		__uintN_xor_u(D, A, B, CHUNKS_1024)
-
-#define	uint128_and(D, A, B)		__uintN_and(D, A, B, CHUNKS_128)
-#define	uint256_and(D, A, B)		__uintN_and(D, A, B, CHUNKS_256)
-#define	uint1024_and(D, A, B)		__uintN_and(D, A, B, CHUNKS_1024)
-
-#define	uint128_and_s(D, A, B)		__uintN_and_s(D, A, B, CHUNKS_128)
-#define	uint256_and_s(D, A, B)		__uintN_and_s(D, A, B, CHUNKS_256)
-#define	uint1024_and_s(D, A, B)		__uintN_and_s(D, A, B, CHUNKS_1024)
-
-#define	uint128_and_u(D, A, B)		__uintN_and_u(D, A, B, CHUNKS_128)
-#define	uint256_and_u(D, A, B)		__uintN_and_u(D, A, B, CHUNKS_256)
-#define	uint1024_and_u(D, A, B)		__uintN_and_u(D, A, B, CHUNKS_1024)
-
-#define	uint128_lsh(D, A)		__uintN_lsh(D, A, CHUNKS_128)
-#define	uint256_lsh(D, A)		__uintN_lsh(D, A, CHUNKS_256)
-#define	uint1024_lsh(D, A)		__uintN_lsh(D, A, CHUNKS_1024)
-
-#define	uint128_rsh(D, A)		__uintN_rsh(D, A, CHUNKS_128)
-#define	uint256_rsh(D, A)		__uintN_rsh(D, A, CHUNKS_256)
-#define	uint1024_rsh(D, A)		__uintN_rsh(D, A, CHUNKS_1024)
+extern	void			__uintN_xor(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
 
 
-#define	uint128_add(D, A, B)		__uintN_add(D, A, B, CHUNKS_128)
-#define	uint256_add(D, A, B)		__uintN_add(D, A, B, CHUNKS_256)
-#define	uint1024_add(D, A, B)		__uintN_add(D, A, B, CHUNKS_1024)
 
-#define	uint128_add_s(D, A, B)		__uintN_add_s(D, A, B, CHUNKS_128)
-#define	uint256_add_s(D, A, B)		__uintN_add_s(D, A, B, CHUNKS_256)
-#define	uint1024_add_s(D, A, B)		__uintN_add_s(D, A, B, CHUNKS_1024)
+#define	uint128_xor_s(D, A, B)	__uintN_xor_s(D, A, B, CHUNKS_128)
+#define	uint256_xor_s(D, A, B)	__uintN_xor_s(D, A, B, CHUNKS_256)
+#define	uint1024_xor_s(D, A, B)	__uintN_xor_s(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_xor_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
+
+
+
+#define	uint128_xor_u(D, A, B)	__uintN_xor_u(D, A, B, CHUNKS_128)
+#define	uint256_xor_u(D, A, B)	__uintN_xor_u(D, A, B, CHUNKS_256)
+#define	uint1024_xor_u(D, A, B)	__uintN_xor_u(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_xor_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
+
+
+
+#define	uint128_and(D, A, B)	__uintN_and(D, A, B, CHUNKS_128)
+#define	uint256_and(D, A, B)	__uintN_and(D, A, B, CHUNKS_256)
+#define	uint1024_and(D, A, B)	__uintN_and(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_and(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
+
+
+#define	uint128_and_s(D, A, B)	__uintN_and_s(D, A, B, CHUNKS_128)
+#define	uint256_and_s(D, A, B)	__uintN_and_s(D, A, B, CHUNKS_256)
+#define	uint1024_and_s(D, A, B)	__uintN_and_s(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_and_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
+
+
+#define	uint128_and_u(D, A, B)	__uintN_and_u(D, A, B, CHUNKS_128)
+#define	uint256_and_u(D, A, B)	__uintN_and_u(D, A, B, CHUNKS_256)
+#define	uint1024_and_u(D, A, B)	__uintN_and_u(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_and_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
+
+
+
+#define	uint128_lsh(D, A)	__uintN_lsh(D, A, CHUNKS_128)
+#define	uint256_lsh(D, A)	__uintN_lsh(D, A, CHUNKS_256)
+#define	uint1024_lsh(D, A)	__uintN_lsh(D, A, CHUNKS_1024)
+
+extern	void    		__uintN_lsh(CHUNK_TYPE *dst, unsigned int shift, unsigned int N);
+
+
+#define	uint128_rsh(D, A)	__uintN_rsh(D, A, CHUNKS_128)
+#define	uint256_rsh(D, A)	__uintN_rsh(D, A, CHUNKS_256)
+#define	uint1024_rsh(D, A)	__uintN_rsh(D, A, CHUNKS_1024)
+
+extern	void			    __uintN_rsh(CHUNK_TYPE *dst, unsigned int shift, unsigned int N);
+
+
+#define	uint128_add(D, A, B)	__uintN_add(D, A, B, CHUNKS_128)
+#define	uint256_add(D, A, B)	__uintN_add(D, A, B, CHUNKS_256)
+#define	uint1024_add(D, A, B)	__uintN_add(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_add(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
+
+
+#define	uint128_add_s(D, A, B)	__uintN_add_s(D, A, B, CHUNKS_128)
+#define	uint256_add_s(D, A, B)	__uintN_add_s(D, A, B, CHUNKS_256)
+#define	uint1024_add_s(D, A, B)	__uintN_add_s(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_add_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
+
 
 #define	uint128_add_ss(D, A, B)		__uintN_add_ss(D, A, B, CHUNKS_128)
 #define	uint256_add_ss(D, A, B)		__uintN_add_ss(D, A, B, CHUNKS_256)
 #define	uint1024_add_ss(D, A, B)	__uintN_add_ss(D, A, B, CHUNKS_1024)
 
+extern	void				__uintN_add_ss(CHUNK_TYPE *dst, const char *a, const char *b, unsigned int N);
+
+
 #define	uint128_add_u(D, A, B)		__uintN_add_u(D, A, B, CHUNKS_128)
 #define	uint256_add_u(D, A, B)		__uintN_add_u(D, A, B, CHUNKS_256)
 #define	uint1024_add_u(D, A, B)		__uintN_add_u(D, A, B, CHUNKS_1024)
+extern	void				__uintN_add_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
 
 
-#define	uint128_mul(D, A, B)		__uintN_mul(D, A, B, CHUNKS_128)
-#define	uint256_mul(D, A, B)		__uintN_mul(D, A, B, CHUNKS_256)
-#define	uint1024_mul(D, A, B)		__uintN_mul(D, A, B, CHUNKS_1024)
+#define	uint128_mul(D, A, B)	__uintN_mul(D, A, B, CHUNKS_128)
+#define	uint256_mul(D, A, B)	__uintN_mul(D, A, B, CHUNKS_256)
+#define	uint1024_mul(D, A, B)	__uintN_mul(D, A, B, CHUNKS_1024)
 
-#define	uint128_mul_s(D, A, B)		__uintN_mul_s(D, A, B, CHUNKS_128)
-#define	uint256_mul_s(D, A, B)		__uintN_mul_s(D, A, B, CHUNKS_256)
-#define	uint1024_mul_s(D, A, B)		__uintN_mul_s(D, A, B, CHUNKS_1024)
+extern	void			__uintN_mul(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
 
-#define	uint128_mul_u(D, A, B)		__uintN_mul_u(D, A, B, CHUNKS_128)
-#define	uint256_mul_u(D, A, B)		__uintN_mul_u(D, A, B, CHUNKS_256)
-#define	uint1024_mul_u(D, A, B)		__uintN_mul_u(D, A, B, CHUNKS_1024)
+
+#define	uint128_mul_s(D, A, B)	__uintN_mul_s(D, A, B, CHUNKS_128)
+#define	uint256_mul_s(D, A, B)	__uintN_mul_s(D, A, B, CHUNKS_256)
+#define	uint1024_mul_s(D, A, B)	__uintN_mul_s(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_mul_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
+
+
+#define	uint128_mul_u(D, A, B)	__uintN_mul_u(D, A, B, CHUNKS_128)
+#define	uint256_mul_u(D, A, B)	__uintN_mul_u(D, A, B, CHUNKS_256)
+#define	uint1024_mul_u(D, A, B)	__uintN_mul_u(D, A, B, CHUNKS_1024)
+
+extern	void			__uintN_mul_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
+
 
 #define	set_uint128(D, S)	__set_uintN(D, S, CHUNKS_128)
 #define	set_uint256(D, S)	__set_uintN(D, S, CHUNKS_256)
 #define	set_uint1024(D, S)	__set_uintN(D, S, CHUNKS_1024)
 
+extern	int			__set_uintN(CHUNK_TYPE *dst, CHUNK_TYPE *src, unsigned int N);
+
+
 #define	set_uint128_s(D, S)	__set_uintN_s(D, S, CHUNKS_128)
 #define	set_uint256_s(D, S)	__set_uintN_s(D, S, CHUNKS_256)
 #define	set_uint1024_s(D, S)	__set_uintN_s(D, S, CHUNKS_1024)
+
+extern	int			__set_uintN_s(CHUNK_TYPE *dst, const char *str, unsigned int N);
+
 
 #define	set_uint128_u(D, S)	__set_uintN_u(D, S, CHUNKS_128)
 #define	set_uint256_u(D, S)	__set_uintN_u(D, S, CHUNKS_256)
 #define	set_uint1024_u(D, S)	__set_uintN_u(D, S, CHUNKS_1024)
 
+extern	int			__set_uintN_u(CHUNK_TYPE *dst, CHUNK_TYPE src, unsigned int N);
+
+
 #define	get_uint128(S)		__get_uintN(S, CHUNKS_128)
 #define	get_uint256(S)		__get_uintN(S, CHUNKS_256)
 #define	get_uint1024(S)		__get_uintN(S, CHUNKS_1024)
 
-
-
-
-
-
-
-extern	void	__uintN_or(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
-extern	void	__uintN_or_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
-extern	void	__uintN_or_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
-
-extern	void	__uintN_xor(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
-extern	void	__uintN_xor_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
-extern	void	__uintN_xor_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
-
-extern	void	__uintN_and(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
-extern	void	__uintN_and_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
-extern	void	__uintN_and_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
-
-extern	void    __uintN_lsh(CHUNK_TYPE *dst, unsigned int shift, unsigned int N);
-
-extern	void    __uintN_rsh(CHUNK_TYPE *dst, unsigned int shift, unsigned int N);
-
-
-extern	void	__uintN_add(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
-extern	void	__uintN_add_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
-extern	void	__uintN_add_ss(CHUNK_TYPE *dst, const char *a, const char *b, unsigned int N);
-extern	void	__uintN_add_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
-
-
-extern	void	__uintN_mul(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE *b, unsigned int N);
-extern	void	__uintN_mul_s(CHUNK_TYPE *dst, CHUNK_TYPE *a, const char *b, unsigned int N);
-extern	void	__uintN_mul_u(CHUNK_TYPE *dst, CHUNK_TYPE *a, CHUNK_TYPE b, unsigned int N);
-
-
-extern	int	__set_uintN(CHUNK_TYPE *dst, CHUNK_TYPE *src, unsigned int N);
-extern	int	__set_uintN_s(CHUNK_TYPE *dst, const char *str, unsigned int N);
-extern	int	__set_uintN_u(CHUNK_TYPE *dst, CHUNK_TYPE src, unsigned int N);
-
-extern	char	*__get_uintN(const CHUNK_TYPE *src, unsigned int N);
+extern	char			*__get_uintN(const CHUNK_TYPE *src, unsigned int N);
 
 
 #endif
--- src/libuint.c
+++ src/libuint.c
@@ -8,23 +8,26 @@
 
 
 
-#define	offset 	"dd268dbcaac550362d98c384c4e576ccc8b1536847b6bbb31023b4c8caee0535"
-#define PRIME	"1000000000000000000000000000000000000000163"
+#define	offset 	"5f7a76758ecc4d32e56d5a591028b74b29fc4223fdada16c3bf34eda3674da9a21d9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004c6d7eb6e73802734510a555f256cc005ae556bde8cc9c6a93b21aff4b16c71ee90b3"
+#define PRIME	"10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018d"
 
 void	fnv_test(CHUNK_TYPE *dst, const char *data, size_t size) {
 
 	size_t i;
-	uint256_t prime;
+	uint128_t prime;
 
-	set_uint256_s(dst, offset);
-	set_uint256_s(prime, PRIME);
+	//set_uint128_s(dst, offset);
+	//set_uint128_s(prime, PRIME);
+	//
+	set_uint128_s(dst, UINT128_MAX);
+	set_uint128_u(prime, 12345678);
 
-	printf("offset: %s\n", get_uint256(dst));
-	printf("prime: %s\n", get_uint256(prime));
+	//printf("offset: %s\n", get_uint1024(dst));
+	//printf("prime: %s\n", get_uint1024(prime));
 
 	for (i = 0; i < size; i++) {
-		uint256_xor_u(dst, dst, data[i]);	
-		uint256_mul(dst, dst, prime);
+		uint128_xor_u(dst, dst, data[i]);	
+		uint128_mul(dst, dst, prime);
 	}
 }
 
@@ -32,31 +35,32 @@ void	fnv_test(CHUNK_TYPE *dst, const char *data, size_t size) {
 int	main() {
 
 
-	uint256_t a, b, c;
+	const char str[] = "Moi mecho mnogo go obi4am";
+	char *bla;
+	size_t size;
+
+	size = 1000;
+
+	bla = malloc(size);
+	memset(bla, 3, size);
+
+	uint128_t a, b, c;
+	uint1024_t x1, x2, x3;
+
+	int i;
+
+	//fnv_test(a, bla, size);
+
+	set_uint1024_s(x1, UINT1024_MAX);
+	set_uint1024_s(x2, UINT1024_MAX);
+
+	for (i = 0; i < 100; i++)
+		uint1024_mul(x3, x1, x2);
+
+	printf("\n   AA = %s\n", get_uint1024(x3));
 
 
-	fnv_test(a, "bla", strlen("bla"));
-
-	printf("\n   AA = %s\n", get_uint256(a));
-		
+	free(bla);
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
